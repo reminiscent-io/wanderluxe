90584cbc577ee7f48af0bc029a3b5e253124db7d
Kevin <36198197+klowe001@users.noreply.github.com>
2025-02-25 20:12:39 -0500

    Update AddExpenseDialog.tsx

--- src/components/trip/budget/AddExpenseDialog.tsx
+++ src/components/trip/budget/AddExpenseDialog.tsx
@@ -9,7 +9,6 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
