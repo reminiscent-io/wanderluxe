d613561630e68393d62fa14b05ea633dbd791a33
Kevin <36198197+klowe001@users.noreply.github.com>
2025-02-25 20:10:46 -0500

    Update HotelSearchInput.tsx

--- src/components/trip/accommodation/HotelSearchInput.tsx
+++ src/components/trip/accommodation/HotelSearchInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Label } from "@/components/ui/label";
 import GooglePlacesAutocomplete from './GooglePlacesAutocomplete';
 import { loadGoogleMapsApi } from '@/utils/loadGoogleMapsApi';
