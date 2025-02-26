064774a9a57a9b8799ba3f62aca09cac48c68b23
Kevin <36198197+klowe001@users.noreply.github.com>
2025-02-25 20:46:58 -0500

    Update CreateTripForm.tsx

--- src/components/trip/create/CreateTripForm.tsx
+++ src/components/trip/create/CreateTripForm.tsx
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import { Button } from "@/components/ui/button";
 import ImageSection from "./ImageSection";
 import TimingSection from "./TimingSection";
 import DestinationInput from "./DestinationInput";
