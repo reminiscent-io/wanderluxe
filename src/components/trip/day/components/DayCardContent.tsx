651bffc7de66e31addc2720bb3bd5a8ea6a40aa1
Kevin <36198197+klowe001@users.noreply.github.com>
2025-03-15 21:43:43 +0000

    remove

--- src/components/trip/day/components/DayCardContent.tsx
+++
@@ -1,4 +0,0 @@
-const handleEditActivityWrapper = (activityId: string) => {
-  console.log("Editing activity with ID:", activityId);
-  onEditActivity?.(activityId) || console.error('onEditActivity is not a function in DayCardContent');
-};
