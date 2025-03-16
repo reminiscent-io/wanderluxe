784099ef25883f8688d734b4fa0dda70b9051f7a
Kevin <36198197+klowe001@users.noreply.github.com>
2025-03-15 20:13:36 -0400

    Update RestaurantReservationDialog.tsx

--- src/components/trip/dining/RestaurantReservationDialog.tsx
+++ src/components/trip/dining/RestaurantReservationDialog.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import RestaurantReservationForm from './RestaurantReservationForm';
@@ -22,10 +21,13 @@ const RestaurantReservationDialog: React.FC<RestaurantReservationDialogProps> = 
 }) => {
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
-      <DialogContent>
+      <DialogContent aria-describedby="restaurant-reservation-description">
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
+        <p id="restaurant-reservation-description" className="sr-only">
+          Please fill out the restaurant reservation form.
+        </p>
         <RestaurantReservationForm
           onSubmit={onSubmit}
           isSubmitting={isSubmitting}
