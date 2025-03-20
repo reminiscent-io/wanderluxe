d2f1c8839068b16c57ae87ce5fadbe423826c536
Kevin <36198197+klowe001@users.noreply.github.com>
2025-03-20 19:43:44 -0400

    Update TimelineContent.tsx

--- src/components/trip/timeline/TimelineContent.tsx
+++ src/components/trip/timeline/TimelineContent.tsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
-import AccommodationGroup from '../accommodation/AccommodationGroup';
 import DayCard from '../day/DayCard';
 import { TripDay } from '@/types/trip';
 import { useQuery } from '@tanstack/react-query';
