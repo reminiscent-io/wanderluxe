d7faa51f6e4e9e0d0546e81277f81d88eba6467a
Kevin <36198197+klowe001@users.noreply.github.com>
2025-03-21 12:55:18 -0400

    Update DiningSection.tsx

--- src/components/trip/dining/DiningSection.tsx
+++ src/components/trip/dining/DiningSection.tsx
@@ -1,6 +1,6 @@
-<AddReservationDialog 
-        isOpen={isAddingReservation}
-        onOpenChange={setIsAddingReservation}
-        onReservationAdded={refetchReservations}
-        tripId={tripId}
-      />
+<AddReservationDialog
+  isOpen={isAddingReservation}
+  onOpenChange={setIsAddingReservation}
+  onReservationAdded={refreshReservations}
+  tripId={tripId}
+/>
