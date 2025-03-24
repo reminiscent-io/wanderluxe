2c8fe4d2bc07db84a97d953486e5f660a71933d5
Kevin <36198197+klowe001@users.noreply.github.com>
2025-03-23 20:36:16 -0400

    Update DayCard.tsx

--- src/components/trip/day/DayCard.tsx
+++ src/components/trip/day/DayCard.tsx
@@ -13,8 +13,9 @@ import { toast } from 'sonner';
 import AccommodationDialog from '@/components/trip/accommodation/AccommodationDialog';
 import ActivityDialogs from '@/components/trip/day/activities/ActivityDialogs';
 import DiningList from '../dining/DiningList';
-import TransportationDialog from '@/components/transportation/TransportationDialog';
-import TransportationListItem from '@/components/transportation/TransportationListItem';
+// Updated import paths for TransportationDialog and TransportationListItem:
+import TransportationDialog from '@/components/trip/transportation/TransportationDialog';
+import TransportationListItem from '@/components/trip/transportation/TransportationListItem';
 
 const initialActivity: ActivityFormData = { 
   title: '', 
