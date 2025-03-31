09c1056353e39ae1bf8df6e3aad71ae3af23152d
Kevin <36198197+klowe001@users.noreply.github.com>
2025-03-30 21:17:47 -0400

    Create cropImageUtil.ts

---
+++ src/utils/cropImageUtil.ts
@@ -0,0 +1,41 @@
+export default async function getCroppedImg(imageSrc: string, crop: any): Promise<string> {
+  const image = await createImage(imageSrc);
+  const canvas = document.createElement('canvas');
+  canvas.width = crop.width;
+  canvas.height = crop.height;
+  const ctx = canvas.getContext('2d');
+
+  ctx?.drawImage(
+    image,
+    crop.x,
+    crop.y,
+    crop.width,
+    crop.height,
+    0,
+    0,
+    crop.width,
+    crop.height
+  );
+
+  return new Promise((resolve, reject) => {
+    canvas.toBlob((blob) => {
+      if (!blob) {
+        reject(new Error('Canvas is empty'));
+        return;
+      }
+      blob.name = 'cropped.jpeg';
+      const croppedImageUrl = window.URL.createObjectURL(blob);
+      resolve(croppedImageUrl);
+    }, 'image/jpeg');
+  });
+}
+
+function createImage(url: string): Promise<HTMLImageElement> {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener('load', () => resolve(image));
+    image.addEventListener('error', (error) => reject(error));
+    image.setAttribute('crossOrigin', 'anonymous');
+    image.src = url;
+  });
+}
