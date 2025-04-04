0c3baa92f5d59dcca796fee7942221bf2c375695
Kevin <36198197+klowe001@users.noreply.github.com>
2025-04-04 13:16:16 +0000

    Restored to '96479c7a092853eff4fd2e1a1543ad457afb0a9f'
    Replit-Restored-To: 96479c7a092853eff4fd2e1a1543ad457afb0a9f

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,4 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Redirect www to non-www with https
-if (window.location.hostname === 'www.wanderluxe.io') {
-  window.location.replace(`https://wanderluxe.io${window.location.pathname}${window.location.search}${window.location.hash}`);
-} else {
-  createRoot(document.getElementById('root')).render(<App />);
-}
+createRoot(document.getElementById('root')).render(<App />);
